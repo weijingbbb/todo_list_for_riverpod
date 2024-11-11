83fefc47cc501d59f1e2ff6d87eb2ae5b5ad5c10
weijingbbb <1047129443@qq.com>
2024-11-11 17:58:34 +0800

    添加主题变化

--- lib/pages/widgets/todo_header.dart
+++ lib/pages/widgets/todo_header.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_list_for_riverpod/pages/providers/active_todo_count/active_todo_count_provider.dart';
+import 'package:todo_list_for_riverpod/pages/providers/theme/theme_provider.dart';
 import 'package:todo_list_for_riverpod/pages/providers/todo_list/todo_list_provider.dart';
 
 class TodoHeader extends ConsumerWidget {
@@ -12,19 +13,30 @@ class TodoHeader extends ConsumerWidget {
     final todos = ref.watch(todoListProvider);
 
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        const Text(
-          'TODO',
-          style: TextStyle(fontSize: 36.0),
+        Row(
+          children: [
+            const Text(
+              'TODO',
+              style: TextStyle(fontSize: 36.0),
+            ),
+            const SizedBox(width: 10),
+            Text(
+              '($active/${todos.length} item${active != 1 ? "s" : ""} left)',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.blue[900],
+              ),
+            ),
+          ],
         ),
-        const SizedBox(width: 10),
-        Text(
-          '($active/${todos.length} item${active != 1 ? "s" : ""} left)',
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Colors.blue[900],
-          ),
-        ),
+        IconButton(
+          onPressed: () {
+            ref.read(themeProvider.notifier).toggleTheme();
+          },
+          icon: const Icon(Icons.light_mode),
+        )
       ],
     );
   }
