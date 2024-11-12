ac92d7a3eda08481048b28bac60b5a1929563632
weijingbbb <1047129443@qq.com>
2024-11-12 11:33:15 +0800

    更新todo_header

--- lib/pages/widgets/todo_header.dart
+++ lib/pages/widgets/todo_header.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:loader_overlay/loader_overlay.dart';
-import 'package:todo_list_for_riverpod/pages/providers/active_todo_count/active_todo_count_provider.dart';
+import 'package:todo_list_for_riverpod/models/todo_model.dart';
 import 'package:todo_list_for_riverpod/pages/providers/theme/theme_provider.dart';
 import 'package:todo_list_for_riverpod/pages/providers/todo_list/todo_list_provider.dart';
 import 'package:todo_list_for_riverpod/pages/providers/todo_list/todo_list_state.dart';
 
-class TodoHeader extends ConsumerWidget {
+class TodoHeader extends ConsumerStatefulWidget {
   const TodoHeader({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final active = ref.watch(activeTodoCountProvider);
+  ConsumerState<TodoHeader> createState() => _TodoHeaderState();
+}
+
+class _TodoHeaderState extends ConsumerState<TodoHeader> {
+  Widget prevTodoCountWidget = const SizedBox.shrink();
+
+  Widget getActiveTodoCount(List<Todo> todos) {
+    final totalCount = todos.length;
+    final activeTodoCount =
+        todos.where((todo) => !todo.completed).toList().length;
+    prevTodoCountWidget = Text(
+      '($activeTodoCount/$totalCount item${activeTodoCount != 1 ? "s" : ""} left)',
+      style: TextStyle(
+        fontSize: 18.0,
+        color: Colors.blue[900],
+      ),
+    );
+    return prevTodoCountWidget;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final todoListState = ref.watch(todoListProvider);
 
-    if (todoListState.status == TodoListStatus.loading) {
-      context.loaderOverlay.show();
-    } else {
-      context.loaderOverlay.hide();
+    switch (todoListState) {
+      case TodoListStateLoading():
+        context.loaderOverlay.show();
+      case _:
+        context.loaderOverlay.hide();
     }
 
     return Row(
@@ -30,31 +51,35 @@ class TodoHeader extends ConsumerWidget {
               style: TextStyle(fontSize: 36.0),
             ),
             const SizedBox(width: 10),
-            Text(
-              '($active/${todoListState.todos.length} item${active != 1 ? "s" : ""} left)',
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.blue[900],
-              ),
-            ),
+            switch (todoListState) {
+              TodoListStateSuccess(todos: var todos) =>
+                getActiveTodoCount(todos),
+              _ => prevTodoCountWidget,
+            },
           ],
         ),
         Row(
           children: [
             IconButton(
-              onPressed: todoListState.status == TodoListStatus.loading
-                  ? null
-                  : () {
-                      ref.read(themeProvider.notifier).toggleTheme();
-                    },
+              onPressed: () {
+                ref.read(themeProvider.notifier).toggleTheme();
+              },
+              // onPressed: todoListState.status == TodoListStatus.loading
+              //     ? null
+              //     : () {
+              //         ref.read(themeProvider.notifier).toggleTheme();
+              //       },
               icon: const Icon(Icons.light_mode),
             ),
             IconButton(
-              onPressed: todoListState.status == TodoListStatus.loading
-                  ? null
-                  : () {
-                      ref.read(todoListProvider.notifier).getTodos();
-                    },
+              onPressed: () {
+                ref.read(todoListProvider.notifier).getTodos();
+              },
+              // onPressed: todoListState.status == TodoListStatus.loading
+              //     ? null
+              //     : () {
+              //         ref.read(todoListProvider.notifier).getTodos();
+              //       },
               icon: const Icon(Icons.refresh),
             ),
           ],
