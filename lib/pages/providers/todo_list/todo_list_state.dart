045e62b1e5c6b69ce1d27c142175cb82de1d527d
weijingbbb <1047129443@qq.com>
2024-11-12 11:02:49 +0800

    更新 todo_list_state为密封类，更新provider的使用逻辑

--- lib/pages/providers/todo_list/todo_list_state.dart
+++ lib/pages/providers/todo_list/todo_list_state.dart
@@ -1,25 +1,68 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:todo_list_for_riverpod/models/todo_model.dart';
 
-part 'todo_list_state.freezed.dart';
+// part 'todo_list_state.freezed.dart';
 
-enum TodoListStatus {
-  initial,
-  loading,
-  success,
-  failure,
+// enum TodoListStatus {
+//   initial,
+//   loading,
+//   success,
+//   failure,
+// }
+
+// @freezed
+// class TodoListState with _$TodoListState {
+//   const factory TodoListState({
+//     required TodoListStatus status,
+//     required List<Todo> todos,
+//     @Default('') String error,
+//   }) = _TodoListState;
+
+//   factory TodoListState.initial() => const TodoListState(
+//         status: TodoListStatus.initial,
+//         todos: [],
+//       );
+// }
+
+// 使用密封类
+sealed class TodoListState {
+  const TodoListState();
 }
 
-@freezed
-class TodoListState with _$TodoListState {
-  const factory TodoListState({
-    required TodoListStatus status,
-    required List<Todo> todos,
-    @Default('') String error,
-  }) = _TodoListState;
+// 初始状态
+final class TodoListStateInitial extends TodoListState {
+  const TodoListStateInitial() : super();
 
-  factory TodoListState.initial() => const TodoListState(
-        status: TodoListStatus.initial,
-        todos: [],
-      );
+  @override
+  String toString() => 'TodoListStateInitial()';
+}
+
+// 加载中
+final class TodoListStateLoading extends TodoListState {
+  const TodoListStateLoading() : super();
+
+  @override
+  String toString() => 'TodoListStateLoading()';
+}
+// 成功
+final class TodoListStateSuccess extends TodoListState {
+  final List<Todo> todos;
+
+  const TodoListStateSuccess({
+    required this.todos,
+  }) : super();
+
+  @override
+  String toString() => 'TodoListStateSuccess(todos: $todos)';
+}
+// 失败
+final class TodoListStateFailure extends TodoListState {
+  final String error;
+
+  const TodoListStateFailure({
+    required this.error,
+  }) : super();
+
+  @override
+  String toString() => 'TodoListStateFailure(error: $error)';
 }
